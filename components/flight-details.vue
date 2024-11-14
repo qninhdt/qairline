df8e14e4fad4925e147130d512881ec5329f3821
JiXiang2108 <124177404+JiXiang2108@users.noreply.github.com>
2024-11-14 22:49:56 +0700

    feat: add flight-details

---
+++ components/flight-details.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="absolute h-[290px] w-[604px] rounded bg-[#FAFAFBFF]">
+    <div
+      class="font-['Sarabun] absolute ml-[21px] mt-[21px] font-[16px] font-medium leading-[26px] text-[#2424FFFF]"
+    >
+      23:15
+    </div>
+
+    <div
+      class="absolute ml-[21px] mt-[127px] font-[14px] font-normal leading-[22px] text-[#171A1FFF]"
+    >
+      2h10"
+    </div>
+    <div
+      class="font-['Sarabun] absolute ml-[21px] mt-[232px] font-[16px] font-medium leading-[26px] text-[#2424FFFF]"
+    >
+      1:25
+    </div>
+    <div
+      class="absolute ml-[81px] mt-[38px] h-[207px] w-[0px] border border-solid border-[#6C6CFFFF]"
+    ></div>
+    <div
+      class="absolute ml-[104px] mt-[21px] font-['Sarabun'] font-[16px] font-medium leading-[25px] text-[#171A1FFF]"
+    >
+      Houston (HOU)
+    </div>
+    <div
+      class="absolute ml-[104px] mt-[49px] font-['Sarabun'] text-sm font-normal leading-[22px] text-[#565E6CFF]"
+    >
+      George Bush Intercontinental Airport, Houston
+    </div>
+    <font-awesome-icon :icon="['fas', 'ticket']" class="h-[20px] w-[20px]" />
+    <div
+      class="absolute ml-[207px] mt-[103px] font-['Sarabun'] text-sm font-normal leading-[22px] text-[#0EA776FF]"
+    >
+      Economy
+    </div>
+    <div class="absolute ml-[106px] mt-[103px] h-[26px] w-[94px] items-center">
+      <div></div>
+      <div
+        class="absolute ml-[20px] mt-[0px] font-['Sarabun'] text-sm font-medium leading-[22px] text-[#0EA776FF]"
+      >
+        CA-6018
+      </div>
+    </div>
+    <div
+      class="dont-normal absolute ml-[129px] mt-[142px] font-['Sarabun'] text-sm leading-[22px] text-[#0EA776FF]"
+    >
+      Baggage
+    </div>
+    <div
+      class="absolute ml-[210px] mt-[142px] font-['Sarabun'] text-sm font-normal leading-[22px] text-[#323842FF]"
+    >
+      2 x 23kg
+    </div>
+    <div
+      class="absolute ml-[129px] mt-[166px] font-['Sarabun'] text-sm font-normal leading-[22px] text-[#0EA776FF]"
+    >
+      Cabin Baggage
+    </div>
+    <div
+      class="absolute ml-[248px] mt-[165px] font-['Sarabun'] text-sm font-normal leading-[22px] text-[#323842FF]"
+    >
+      1 x 7kg
+    </div>
+    <div
+      class="dont-normal absolute ml-[355px] mt-[142px] font-['Sarabun'] text-sm leading-[22px] text-[#0EA776FF]"
+    >
+      Airbus
+    </div>
+    <div
+      class="dont-normal absolute ml-[421px] mt-[142px] font-['Sarabun'] text-sm leading-[22px] text-[#323842FF]"
+    >
+      A320
+    </div>
+    <div
+      class="dont-normal absolute ml-[355px] mt-[165px] font-['Sarabun'] text-sm leading-[22px] text-[#323842FF]"
+    >
+      3-3 seat layout
+    </div>
+    <div
+      class="dont-normal absolute ml-[355px] mt-[189px] font-['Sarabun'] text-sm leading-[22px] text-[#323842FF]"
+    >
+      29 inches Seat pitch (standard)
+    </div>
+    <div
+      class="absolute ml-[104px] mt-[232px] font-['Sarabun'] font-[16px] font-medium leading-[25px] text-[#171A1FFF]"
+    >
+      Las Vegas (LAS)
+    </div>
+    <div
+      class="absolute ml-[104px] mt-[260px] font-['Sarabun'] text-sm font-normal leading-[22px] text-[#565E6CFF]"
+    >
+      McCarran International Airport, Las Vegas
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
