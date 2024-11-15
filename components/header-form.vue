b60b38b446bc9202bf959d939ea4bd98fe17ace0
JiXiang2108 <124177404+JiXiang2108@users.noreply.github.com>
2024-11-15 15:40:28 +0700

    feat: add header

---
+++ components/header-form.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="absolute ml-[0px] mt-[0px] h-[64px] w-[1440px] rounded-none bg-white"
+  >
+    <div>
+      <router-link
+        to="/"
+        class="font-['Sarabun] absolute ml-[179px] mt-[19px] font-[20px] font-semibold leading-[30px] text-[#6C6CFFFF]"
+        >QAirline</router-link
+      >
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
