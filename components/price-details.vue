c3198089a1352c71eaec0ef0d2ad10752d421c3f
JiXiang2108 <124177404+JiXiang2108@users.noreply.github.com>
2024-11-24 17:03:05 +0700

    feat: add price details

---
+++ components/price-details.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="rounded-8px absolute h-[280px] w-[328px] bg-white">
+    <div
+      class="absolute ml-[37px] mt-[16px] text-[20px] font-bold leading-[30px] text-[#323842FF] text-['Sarabun']"
+    >
+      Price details
+    </div>
+    <hr
+      class="absolute ml-[0px] mt-[59px] h-[0px] w-[328px] border border-solid border-[#DEE1E6FF]"
+    />
+    <div
+      class="absolute ml-[20px] mt-[76px] text-[14px] font-medium leading-[22px] text-[#323842FF] text-['Sarabun']"
+    >
+      Adult basic fee
+    </div>
+    <div
+      class="absolute ml-[277px] mt-[76px] text-[14px] font-medium leading-[22px] text-[#323842FF] text-['Sarabun']"
+    >
+      $150
+    </div>
+    <div
+      class="absolute ml-[20px] mt-[108px] text-[14px] font-medium leading-[22px] text-[#323842FF] text-['Sarabun']"
+    >
+      Tax
+    </div>
+    <div
+      class="absolute ml-[254px] mt-[108px] text-[14px] font-medium leading-[22px] text-[#323842FF] text-['Sarabun']"
+    >
+      Included
+    </div>
+    <div
+      class="absolute ml-[20px] mt-[140px] text-[14px] font-medium leading-[22px] text-[#323842FF] text-['Sarabun']"
+    >
+      Regular total fee
+    </div>
+    <div
+      class="absolute ml-[277px] mt-[140px] text-[14px] font-medium leading-[22px] text-[#323842FF] text-['Sarabun']"
+    >
+      $150
+    </div>
+    <div
+      class="absolute ml-[20px] mt-[172px] text-[14px] font-medium leading-[22px] text-[#FF912BFF] text-['Sarabun']"
+    >
+      Save
+    </div>
+    <div
+      class="absolute ml-[272px] mt-[172px] text-[14px] font-medium leading-[22px] text-[#FF912BFF] text-['Sarabun']"
+    >
+      -$1.5
+    </div>
+    <hr
+      class="absolute ml-[20px] mt-[214px] h-[0px] w-[288px] border border-solid border-[#BCC1CAFF]"
+    />
+    <div
+      class="absolute ml-[20px] mt-[230px] text-[14px] font-medium leading-[22px] text-[#323842FF] text-['Sarabun']"
+    >
+      Total
+    </div>
+    <div
+      class="absolute ml-[259px] mt-[230px] text-[16px] font-medium leading-[22px] text-[#0EA776FF] text-['Sarabun']"
+    >
+      $148.5
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
