bb20f58887c131c87c333eabf1a608dfe08bf366
qninhdt <qndt123@gmail.com>
2024-12-16 03:20:41 +0700

    feat: lmao

---
+++ components/chess-doodle.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import 'css-doodle'
+
+const { grid = 8 } = defineProps(['grid'])
+</script>
+
+<template>
+  <css-doodle :class="$attrs.class">
+    @grid: {{ grid }} / 80%; @random { border-left: 1px solid
+    hsl(var(--foreground)); } @random { border-top: 1px solid
+    hsl(var(--foreground)); } @random(.25) { background: linear-gradient(
+    @p(hsl(var(--primary)), hsl(var(--primary)), hsl(var(--foreground))), @lp )
+    50% / @r(60%) @lr no-repeat; } @random { filter: drop-shadow(0 0 10px
+    hsl(var(--background))); }
+  </css-doodle>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,102 @@
-<script setup lang="ts">
-import { OpenAI } from 'openai'
+<script lang="ts" setup>
+import { EffectCards } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/vue'
 
-const result = ref<string | null>('')
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/effect-cards'
 
-onMounted(async () => {
-  console.log(process.env.OPENAI_API_KEY)
-  const openai = new OpenAI({
-    dangerouslyAllowBrowser: true,
-    apiKey:
-      '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'
-  })
+import 'css-doodle'
 
-  const tools: any[] = [
-    {
-      type: 'function',
-      function: {
-        name: 'get_weather',
-        parameters: {
-          type: 'object',
-          properties: {
-            location: { type: 'string' }
-          }
-        }
-      }
-    }
-  ]
-  const response = await openai.chat.completions.create({
-    model: 'gpt-4o',
-    messages: [
-      { role: 'user', content: "What's the weather like in Paris today?" }
-    ],
-    tools
-  })
-  console.log(response.choices[0].message.tool_calls[0].function)
-})
+const images = [
+  {
+    url: 'images/hanoi.jpg',
+    title: 'Hà Nội'
+  },
+  {
+    url: 'images/newyork.jpg',
+    title: 'New York'
+  },
+  {
+    url: 'images/paris.jpg',
+    title: 'Paris'
+  },
+  {
+    url: 'images/bangkok.jpg',
+    title: 'Bangkok'
+  }
+]
+
+const activeIndex = ref(0)
+
+// const numberOfPassengers = ref(1)
+// const isBusinessClass = ref(false)
+
+const onChange = (swiper: any) => {
+  activeIndex.value = swiper.activeIndex
+}
 </script>
 
 <template>
-  <div>{{ result }}</div>
+  <div class="h-[100vh] w-[100vw] px-8 pt-40">
+    <div
+      class="mx-auto flex items-center justify-between px-0 lg:px-32 xl:px-[20rem]"
+    >
+      <div class="w-full px-4">
+        <h1
+          class="mb-4 text-3xl font-bold uppercase lg:text-4xl lg:leading-[3.5rem]"
+        >
+          <span
+            >Trải nghiệm <span class="text-primary">chuyến bay</span><br />
+            <span class="text-primary">thượng hạng</span> cùng QAIRLINE!
+          </span>
+        </h1>
+        <p class="mt-2 text-xl text-muted-foreground">
+          Bắt đầu chuyến hành trình của bạn ngay hôm nay!
+        </p>
+      </div>
+      <div class="relative hidden w-fit lg:block">
+        <swiper
+          class="h-[256px] w-[256px] rotate-[10deg] overflow-hidden rounded-2xl"
+          :effect="'cards'"
+          :grab-cursor="true"
+          :modules="[EffectCards]"
+          @slide-change="onChange"
+        >
+          <swiper-slide v-for="(image, index) in images" :key="index">
+            <div class="h-[256px] w-[256px] overflow-hidden rounded-2xl">
+              <img
+                :src="image.url"
+                alt="image"
+                class="h-[256px] w-[256px] object-cover"
+              />
+            </div>
+          </swiper-slide>
+        </swiper>
+        <falling-doodle class="absolute left-[-10%] top-[-50%] z-[-10]" />
+
+        <div
+          class="absolute left-[-10%] top-[40%] z-10 flex h-[50px] w-[50px] items-center justify-center rounded-full border-[1px] border-primary bg-background"
+        >
+          <animated-icon id="play" :size="32" />
+        </div>
+      </div>
+    </div>
+    <booking-form />
+    <!-- <div class="bottom-0 left-0">
+      <city-doodle />
+    </div> -->
+  </div>
 </template>
+
+<style>
+.swiper-slide-shadow {
+  border-radius: 1rem;
+  background-color: color-mix(
+    in srgb,
+    hsl(var(--primary)) 50%,
+    transparent
+  ) !important;
+}
+</style>
